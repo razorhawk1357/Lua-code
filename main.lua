15f800b35783b5e59094e5ccb8e31422a01bbdef
razorhawk1357 <razorhawk1357@gmail.com>
2015-07-12 13:21:38 -0700

    added files from mobile engine

--- main.lua
+++ main.lua
@@ -5,16 +5,40 @@
 -----------------------------------------------------------------------------------------
 
 -- requires
-local GameLoop = require ("tools.gameLoop")
+local GameLoop 		= require ("tools.gameLoop")
+local gsm      		= require ("tools.gsm")
+local obm      		= require ("tools.obm")
+local perspective 	= require ("tools.perspective")
+local gfx       	= require ("tools.gfx")
 
 -- local init
-local main = {}
+local main 		= {}
+local camera 	= perspective.createView(4)
+
 -- global init
 GAME_TIME = 0 -- global game ticker
 gameLoop  = GameLoop:new()
-gameLoop:start()
+
+display.setStatusBar( display.HiddenStatusBar )
+display.setDefault("magTextureFilter", "nearest" )
+
 -- functions
+function main:load()
+	gameLoop:start()
+	gameLoop:addLoop(self)
+
+	gsm:load()
+	obm:load()
+end
 function main:tick(dt)
 	GAME_TIME = GAME_TIME + dt
 end
-gameLoop:addLoop(main)
+
+function getCamera()
+	return camera
+end
+function getGFX()
+	return gfx
+end
+
+main:load()
